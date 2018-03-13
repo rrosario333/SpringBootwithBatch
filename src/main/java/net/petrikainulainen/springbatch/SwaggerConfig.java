84a69392bf19667b4c321509b44889034774d93e
Robert Rosario <Robert Rosario@RobertRosario>
2018-03-13 01:00:46 +0000

    pathprovider

--- src/main/java/net/petrikainulainen/springbatch/SwaggerConfig.java
+++ src/main/java/net/petrikainulainen/springbatch/SwaggerConfig.java
@@ -4,6 +4,8 @@ import static com.google.common.collect.Lists.newArrayList;
 
 import java.util.Collections;
 
+import javax.servlet.ServletContext;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,6 +17,7 @@ import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.paths.RelativePathProvider;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -22,13 +25,13 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfig {
 
-	 @Bean
-	    public Docket api() { 
+		@Bean
+	    public Docket api(ServletContext servletContext) { 
 	        return new Docket(DocumentationType.SWAGGER_2)  
 	          .select()                                  
-	          .apis(RequestHandlerSelectors.any())              
-	          .paths(PathSelectors.any())                          
-	          .build();                                           
+	          .apis(RequestHandlerSelectors.any())            
+	          .paths(PathSelectors.any())                         
+	          .build().apiInfo(apiInfo()).pathProvider(new RelativePathProvider(servletContext));                                           
 	    }
 
 	private ApiInfo apiInfo() {
